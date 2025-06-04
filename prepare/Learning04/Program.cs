4e221b3f8c9912441200d7526ee54e9b6fb567c8
kaiyajoye <makaiyanunn@mail.weber.edu>
2025-06-04 16:51:05 -0600

    -m

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Data;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment Assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(Assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,28 +1,28 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    // (),(void is the return type),(Main is the name of the function),(string[] is the data type) 
-    // Main in required in C#, case sensitive
-    {
-        // get the radius of a circle from the user
-        Console.Write("Please enter the radius: ");
-        string text = Console.ReadLine();
-        double radius = double.Parse(text);
+// class Program
+// {
+//     static void Main(string[] args)
+//     // (),(void is the return type),(Main is the name of the function),(string[] is the data type) 
+//     // Main in required in C#, case sensitive
+//     {
+//         // get the radius of a circle from the user
+//         Console.Write("Please enter the radius: ");
+//         string text = Console.ReadLine();
+//         double radius = double.Parse(text);
 
-        // compute area of circle
-        double area = Math.PI * radius * radius;
+//         // compute area of circle
+//         double area = Math.PI * radius * radius;
 
-        // display the area for the user to see
-        Console.WriteLine($"Area of the circle: {area}");
+//         // display the area for the user to see
+//         Console.WriteLine($"Area of the circle: {area}");
 
-        int x = 3;
-        string s = "Goodbye";
-        float f = 3.14F;    // "F" is needed to tell the compiler to treat 3.14 as a float isntead of a double
-        double d = 5.21;
-        long n = 25;
+//         int x = 3;
+//         string s = "Goodbye";
+//         float f = 3.14F;    // "F" is needed to tell the compiler to treat 3.14 as a float isntead of a double
+//         double d = 5.21;
+//         long n = 25;
 
-        Console.WriteLine($"Hello, {s} Sandbox World! {x} {f} {d}");
-    }
-}
+//         Console.WriteLine($"Hello, {s} Sandbox World! {x} {f} {d}");
+//     }
+// }
